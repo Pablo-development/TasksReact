89501e089f2fd23d8a206dd393c9aa44a473f9c8
Pablo <pablo.mikolaiczyki@gmail.com>
2026-04-22 00:24:42 -0300

    adicionado funcionalidade de exclusão de tasks e mudanca de estado para concluido

--- projetos/React/TasksReact-master/src/App.jsx
+++ projetos/React/TasksReact-master/src/App.jsx
@@ -22,12 +22,31 @@ function App(){
     },
   ]);
 
+  function onTaskClick(taskId){
+    const newTask = tasks.map((task) => {
+      console.log(task)
+      if(task.id === taskId){
+        //Aqui, caso o id recebido seja o mesmo da iteração, atualizamos o boolean
+        //falso para true
+        return {...task, isCompleted: !task.isCompleted}
+      }
+      return task;
+    })
+    setTasks(newTask);
+  }
+
+  function onDeleteTaskClick(taskId){
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+  
+
   return (
     <div className="h-screen w-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de tarefas</h1>
         <AddTask/>
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
     </div>
   );
--- projetos/React/TasksReact-master/src/components/Tasks.jsx
+++ projetos/React/TasksReact-master/src/components/Tasks.jsx
@@ -1,13 +1,22 @@
+import { ChevronsRightIcon, TrashIcon } from "lucide-react";
+
 //recebe props como parametro, sendo props as tasks
 function Tasks(props){
-    //no console do navegador, podemos ver os objetos
-    console.log(props);
-    
+    //space-y-4 faz com que cada elemento dentro da UL tenha um espaçamento entre sí
     return (
-        <ul>
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {props.tasks.map((task) => (
-            <li key={task.id} className="bg-slate-700 text-white p-2">
-                {task.title}
+            <li key={task.id} className="flex gap-2 ">
+                <button onClick={() => props.onTaskClick(task.id)}
+                className={`bg-slate-700 text-white p-2 w-full ${task.isCompleted && "line-through"}`}>
+                    {task.title}
+                </button>
+                <button className="bg-slate-700 text-white p-2 ">
+                    <ChevronsRightIcon />
+                </button>
+                <button onClick= {() => props.onDeleteTaskClick(task.id)} className="bg-slate-700 text-white p-2 ">
+                    <TrashIcon />
+                </button>
             </li>
             ))}
         </ul>
