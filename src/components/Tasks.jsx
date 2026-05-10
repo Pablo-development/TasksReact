cacef869c2cb09d4e4f393e2ed80c977b9fa2360
Pablo <pablo.mikolaiczyki@gmail.com>
2026-05-10 19:14:51 -0300

    Implementação do React-routes, alem da criacao inicial da pagina TaskPage

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,38 @@
+import { ChevronsRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+function Tasks({tasks, onTaskClick, onDeleteTaskClick}){
+    const navigate = useNavigate()
+    
+    function onSeeDetailsClick(task){
+        const query = new URLSearchParams();
+        query.set("title", task.title);
+        query.set("description", task.description);
+        navigate(`/task?${query.toString()}`);
+    }
+
+    function testeNavigate(){
+        navigate(`/task`)
+    }
+    //space-y-4 faz com que cada elemento dentro da UL tenha um espaçamento entre sí
+    return (
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+            {tasks.map((task) => (
+            <li key={task.id} className="flex gap-2 ">
+                <button onClick={() => onTaskClick(task.id)}
+                className={`bg-slate-700 text-white p-2 w-full ${task.isCompleted && "line-through"}`}>
+                    {task.title}
+                </button>
+                <button onClick={() => testeNavigate()} className="bg-slate-700 text-white p-2 ">
+                    <ChevronsRightIcon />
+                </button>
+                <button onClick= {() => onDeleteTaskClick(task.id)} className="bg-slate-700 text-white p-2 ">
+                    <TrashIcon />
+                </button>
+            </li>
+            ))}
+        </ul>
+    );  
+}
+
+export default Tasks
---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,17 @@
+import { useSearchParams } from "react-router-dom";
+
+function TaskPage(){
+    const [searchParams] = useSearchParams();
+    const title = searchParams.get("title")
+    const description  = searchParams.get("description")
+
+    return (
+        <div className="bg-slate-500 w-screen h-screen justify-center flex p-6">
+            <div className="w-[500px] space-y-4 space-x-2 bg-slate-200">
+                <h1 className="text-black">{title}</h1>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
+export default TaskPage;
