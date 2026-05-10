b88499e52d2b0bd11acfd36621ddabd0a7fe5227
Pablo <pablo.mikolaiczyki@gmail.com>
2026-05-10 19:20:36 -0300

    Implementação do React-routes, alem da criacao inicial da pagina TaskPage

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -9,6 +9,7 @@ function TaskPage(){
         <div className="bg-slate-500 w-screen h-screen justify-center flex p-6">
             <div className="w-[500px] space-y-4 space-x-2 bg-slate-200">
                 <h1 className="text-black">{title}</h1>
+                
                 <p>{description}</p>
             </div>
         </div>
